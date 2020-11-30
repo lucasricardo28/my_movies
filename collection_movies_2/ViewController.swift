51656c1ca05d77f30e94d988f13b53d83d3bb180
Ricardo Lucas <lucasricardo28@gmail.com>
2020-11-30 19:27:23 -0300

    Initial Commit

---
+++ collection_movies_2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  collection_movies_2
+//
+//  Created by Ricardo Lucas De Sousa Martins on 30/11/20.
+//  Copyright © 2020 Ricardo Lucas De Sousa Martins. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
